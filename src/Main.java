bb230fea48ca5f5ae5671d1d6c84706105dc32b3
Bastanumet <artur.kaharlytskyi@gmail.com>
2022-09-14 00:55:44 +0300

    Test
    Test

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("What is Bitcoin price today?");
-        float btcPrice = scanner.nextFloat();
+        while (true) {
+            try {
+                Scanner scanner = new Scanner(System.in);
 
-        System.out.println("How much $ do you have?");
-        float usdAmount = scanner.nextFloat();
+                System.out.println("What is Bitcoin price today?");
+                float btcPrice = scanner.nextFloat();
 
-        float canBuy = usdAmount/btcPrice; //result
+                System.out.println("How much $ do you have?");
+                float usdAmount = scanner.nextFloat();
 
-        System.out.println("You can buy " + canBuy + " BTC");
+                float canBuy = usdAmount / btcPrice; // result
+
+                System.out.println("You can buy " + canBuy + " BTC");
+                scanner.close();
+                break;
+            }
+
+            catch (Exception e) {
+                System.err.println("Please write number, not text");
+            }
+
+        }
     }
 }
